3e21453cb01929c9b8f7b38b947deaab3eeb3030
dev.asx666 <bezhuk.sv@gmail.com>
2020-06-08 16:59:03 +0300

    Reffactoring auth user, by using phone number

--- lib/screens/LoginScreens/LoginScreen.dart
+++ lib/screens/LoginScreens/LoginScreen.dart
@@ -9,7 +9,6 @@ import 'package:StreetCoffee/screens/LoginScreens/RegistrationScreen.dart';
 class LoginScreen extends StatefulWidget {
   @override
   _LoginScreenState createState() => _LoginScreenState();
-
 }
 
 class _LoginScreenState extends State<LoginScreen> {
--- lib/utilities/Auth/AuthUser.dart
+++ lib/utilities/Auth/AuthUser.dart
@@ -2,6 +2,56 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:StreetCoffee/screens/MenuDashboardLayout/MenuDashboardLayout.dart';
 import 'package:flutter/material.dart';
 
+class AuthUserLogic {
+  void checkUserEnterCode(BuildContext context, TextEditingController _codeController, FirebaseAuth _auth, String verificationId) async {
+    final code = _codeController.text.trim();
+    AuthCredential credential = PhoneAuthProvider.getCredential(verificationId: verificationId, smsCode: code);
+
+    AuthResult result;
+    
+    try {
+      result = await _auth.signInWithCredential(credential);
+    }
+    catch (PlatformException) {
+      print("Invalid Enter Code!!!");
+
+      return;
+    }
+
+    FirebaseUser user = result.user;
+
+    if (user != null) {
+      Navigator.push(context, MaterialPageRoute(
+        builder: (context) => MenuDashboardLayout(user: user,)
+      ));
+    } else {
+      print("Error");
+
+      return;
+    }
+  }
+
+  void verificationNumberSuccess(AuthCredential credential, BuildContext context, FirebaseAuth _auth) async {
+    Navigator.of(context).pop();
+
+    AuthResult result = await _auth.signInWithCredential(credential);
+
+    FirebaseUser user = result.user;
+
+    if (user != null){
+      Navigator.push(context, MaterialPageRoute(
+        builder: (context) => MenuDashboardLayout(user: user,)
+      ));
+    } else {
+      print("Error");
+    }
+  }
+
+  void verificationFailed(AuthException exception) {
+    print(exception.message);
+  }
+}
+
 class AuthUser {
   final _codeController = TextEditingController();
 
@@ -11,32 +61,19 @@ class AuthUser {
     _auth.verifyPhoneNumber(
         phoneNumber: phone,
         timeout: Duration(seconds: 60),
-        verificationCompleted: (AuthCredential credential) async {
-          Navigator.of(context).pop();
-
-          AuthResult result = await _auth.signInWithCredential(credential);
-
-          FirebaseUser user = result.user;
-
-          if (user != null){
-            Navigator.push(context, MaterialPageRoute(
-              builder: (context) => MenuDashboardLayout(user: user,)
-            ));
-          } else {
-            print("Error");
-          }
+        verificationCompleted: (AuthCredential credential) {
+          AuthUserLogic().verificationNumberSuccess(credential, context, _auth);
         },
-        verificationFailed: (AuthException exception){
-          print(exception.message);
-          print(phone);
+        verificationFailed: (AuthException exception) {
+          AuthUserLogic().verificationFailed(exception);
         },
-        codeSent: (String verificationId, [int forceResendingToken]){
+        codeSent: (String verificationId, [int forceResendingToken]) {
           showDialog(
             context: context,
             barrierDismissible: false,
             builder: (context) {
               return AlertDialog(
-                title: Text("Give the code?"),
+                title: Text("Введіть код з смс"),
                 content: Column(
                   mainAxisSize: MainAxisSize.min,
                   children: <Widget>[
@@ -47,24 +84,16 @@ class AuthUser {
                 ),
                 actions: <Widget>[
                   FlatButton(
-                    child: Text("Confirm"),
+                    child: Text("Пітвердити!"),
                     textColor: Colors.white,
-                    color: Colors.blue,
-                    onPressed: () async {
-                      final code = _codeController.text.trim();
-                      AuthCredential credential = PhoneAuthProvider.getCredential(verificationId: verificationId, smsCode: code);
-
-                      AuthResult result = await _auth.signInWithCredential(credential);
-
-                      FirebaseUser user = result.user;
-
-                      if (user != null) {
-                        Navigator.push(context, MaterialPageRoute(
-                            builder: (context) => MenuDashboardLayout(user: user,)
-                        ));
-                      } else {
-                        print("Error");
-                      }
+                    color: Color(0xFF04B47C),
+                    onPressed: () {
+                      AuthUserLogic().checkUserEnterCode(
+                        context,
+                        _codeController,
+                        _auth,
+                        verificationId
+                      );
                     },
                   )
                 ],
