4562ca6cabb3eb098eec944d727df1bac1efe718
dev.asx666 <bezhuk.sv@gmail.com>
2020-06-14 16:20:40 +0300

    Get image from server in url by image from firebase db

--- lib/utilities/Widget/Cards.dart
+++ lib/utilities/Widget/Cards.dart
@@ -1,47 +1,83 @@
 import 'dart:ui';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:rxdart/subjects.dart';
 
-Widget CardsDashboard() {
+class GetDateImage {
+  var subject = new PublishSubject<String>();
+
+  PublishSubject<String> _readDate(String childName) {
+    final DBRef = FirebaseDatabase.instance.reference().child(childName);
+    
+    DBRef.once().then((DataSnapshot dataSnapshot) {
+      subject.add(dataSnapshot.value.toString());
+    });
+
+    return subject;
+  }
+
+}
+
+class RenderCards {
+
+  List<Widget> renderCard(List<String> url) {
+    List<Widget> card = [ ];
+
+    RegExp regExp = new RegExp(
+      r"http:\/\/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/=]*)?",
+      caseSensitive: false,
+      multiLine: false,
+    );
+
+    for (int i = 0; i < url.length; i++) {
+      card.add( 
+        Card(
+          elevation: 4.0,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+          clipBehavior: Clip.antiAlias,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget> [
+              Image.network(
+                regExp.stringMatch(url[ i ]).toString(),
+                fit: BoxFit.contain,
+                height: 480.0,
+                width: 320.0,
+              )
+            ],
+          ),
+        )
+      );
+    }
+    return card;
+  }
+}
+
+Widget CardsDashboard(String childDataName) {
+  var subject = new PublishSubject<String>();
+  var getImage = new GetDateImage();
+  
+  subject = getImage._readDate(childDataName);
+  
   return Container(
     height: 500,
-    child: PageView(
-    controller: PageController(viewportFraction: 1.0),
-    scrollDirection: Axis.horizontal,
-    pageSnapping: true,
-      children: <Widget>[
-        Card(
-          elevation: 4.0,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
-          clipBehavior: Clip.antiAlias,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[ 
-              Image.network(
-                "http://185.86.78.46/news_1.jpeg",
-                fit: BoxFit.contain,
-                  height: 480.0,
-                  width: 320.0,
-              )
-            ],
-          ),
-        ),
-        Card(
-          elevation: 4.0,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
-          clipBehavior: Clip.antiAlias,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[ 
-              Image.network(
-                "http://185.86.78.46/news_1.jpeg",
-                fit: BoxFit.contain,
-                  height: 480.0,
-                  width: 320.0,
-              )
-            ],
-          ),
-        ),
-      ],
+    child: StreamBuilder(
+      stream: subject.stream,
+      builder: (context, snapsot) {
+        if (snapsot.data == null) {
+          return CircularProgressIndicator();
+        }
+
+        var urls = snapsot.data.split(',');
+
+        return PageView(
+          controller: PageController(viewportFraction: 1.0),
+          scrollDirection: Axis.horizontal,
+          pageSnapping: true,
+          children: 
+            RenderCards().renderCard(urls)
+        );
+      },
     ),
   );
 }
