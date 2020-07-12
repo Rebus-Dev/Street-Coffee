99e9286e99d97e5a4c23fa330aa780b88ff29b96
dev.asx666 <bezhuk.sv@gmail.com>
2020-07-12 03:00:49 +0300

    Fix visible card in menu

--- lib/utilities/Widget/Cards.dart
+++ lib/utilities/Widget/Cards.dart
@@ -37,7 +37,7 @@ class GetDateImage {
 
   PublishSubject<Map<String, dynamic>> _readDate(String childName) {
     final DBRef = FirebaseDatabase.instance.reference().child(childName);
-    
+
     String url = '';
     String name = '';
 
@@ -45,7 +45,7 @@ class GetDateImage {
       url = dataSnapshot.value["image"];
       name = dataSnapshot.value["name"];
 
-      Map<String, dynamic> tmpValue = { "image": url, "name": name };
+      Map<String, dynamic> tmpValue = {"image": url, "name": name};
 
       subject.add(tmpValue);
     });
@@ -68,21 +68,23 @@ class GetDateImage {
       bool ifInList = new RegExp('/list').hasMatch(childName);
       if (ifInList) {
         for (final data in dataSnapshot.value) {
-          
           url = data["image"];
           name = data["name"];
           descr = data["descr"];
           price = data["price"];
 
-          Map<String, dynamic> tmpValue = { "image": url, "name": name, "descr" : descr, "price" : price };
-          
+          Map<String, dynamic> tmpValue = {
+            "image": url,
+            "name": name,
+            "descr": descr,
+            "price": price
+          };
+
           tempList.add(tmpValue);
-          
         }
 
         subject.add(tempList);
-      }      
-
+      }
     });
 
     return subject;
@@ -98,62 +100,54 @@ class GetDateImage {
 
     return subject;
   }
-
 }
 
 class RenderCards {
-
   List<Widget> renderCard(List<String> url) {
-    List<Widget> card = [ ];
+    List<Widget> card = [];
 
     for (int i = 0; i < url.length; i++) {
-      card.add( 
-        Card(
-          elevation: 4.0,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
-          clipBehavior: Clip.antiAlias,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget> [
-              Image.network(
-                regExp.stringMatch(url[ i ]).toString(),
-                fit: BoxFit.contain,
-                height: 480.0,
-                width: 320.0,
-              )
-            ],
-          ),
-        )
-      );
+      card.add(Card(
+        elevation: 4.0,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Image.network(
+              regExp.stringMatch(url[i]).toString(),
+              fit: BoxFit.contain,
+              height: 480.0,
+              width: 320.0,
+            )
+          ],
+        ),
+      ));
     }
     return card;
   }
-  
+
   Widget renderRow(Map<String, dynamic> snapsot) {
     return new Row(
       crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget> [
+      children: <Widget>[
         new Padding(
-          padding: new EdgeInsets.only(left: 20.0, right: 0.0),
-          child: new CircleAvatar(
-            radius: 35.0, 
-            backgroundImage: NetworkImage(regExp.stringMatch(snapsot["image"]).toString())
-          )
-        ),
-        new Padding(
-          padding: new EdgeInsets.only(left: 20.0, right: 0.0),
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new Text(
-                snapsot["name"],
-                style: new TextStyle(
-                  fontSize: 20.0, 
-                  color: Colors.white70
-                )
-              ),
-            ],
-          )
+            padding: new EdgeInsets.only(left: 20.0, right: 0.0),
+            child: new CircleAvatar(
+                radius: 35.0,
+                backgroundImage: NetworkImage(
+                    regExp.stringMatch(snapsot["image"]).toString()))),
+        Expanded(
+          child: new Padding(
+              padding: new EdgeInsets.only(left: 20.0, right: 0.0),
+              child: new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new Text(snapsot["name"],
+                      style:
+                          new TextStyle(fontSize: 20.0, color: Colors.white70)),
+                ],
+              )),
         )
       ],
     );
@@ -162,72 +156,62 @@ class RenderCards {
   List<Widget> renderRowArray(List<Map<String, dynamic>> snapsot) {
     List<Widget> rows = List();
 
-    for(int item = 0; item < snapsot.length; item++) {
-      rows.add(
-        Card(
-          elevation: 5,
-          child: Container(
-            height: 120.0,
-            child: Row(
-              children: <Widget>[
-                Container(
-                  height: 120.0,
-                  width: 80.0,
-                  decoration: BoxDecoration(
+    for (int item = 0; item < snapsot.length; item++) {
+      rows.add(Card(
+        elevation: 5,
+        child: Container(
+          height: 120.0,
+          child: Row(
+            children: <Widget>[
+              Container(
+                height: 120.0,
+                width: 80.0,
+                decoration: BoxDecoration(
                     borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(5),
-                      topLeft: Radius.circular(5)
-                    ),
+                        bottomLeft: Radius.circular(5),
+                        topLeft: Radius.circular(5)),
                     image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: NetworkImage(snapsot[item]["image"])
-                    )
-                  ),
-                ),
-                Container(
-                  height: 120,
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(10, 2, 0, 0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          snapsot[item]["name"]
+                        fit: BoxFit.cover,
+                        image: NetworkImage(snapsot[item]["image"]))),
+              ),
+              Container(
+                height: 120,
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(10, 2, 0, 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(snapsot[item]["name"]),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(0, 3, 0, 3),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.teal),
+                          ),
+                          child: Text(snapsot[item]["price"],
+                              textAlign: TextAlign.center),
                         ),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(0, 3, 0, 3),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.teal),
-                            ),
-                            child: Text(
-                              snapsot[item]["price"],
-                              textAlign: TextAlign.center
-                            ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(0, 5, 0, 2),
+                        child: Container(
+                          width: 260,
+                          child: Text(
+                            snapsot[item]["descr"],
+                            style: TextStyle(
+                                fontSize: 12,
+                                color: Color.fromARGB(255, 48, 48, 54)),
                           ),
                         ),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(0, 5, 0, 2),
-                            child: Container(
-                              width: 260,
-                              child: Text(
-                                snapsot[item]["descr"],
-                                style: TextStyle(
-                                  fontSize: 12,
-                                  color: Color.fromARGB(255, 48, 48, 54)
-                                ),
-                              ),
-                            ),
-                        )
-                      ],
-                    ),
+                      )
+                    ],
                   ),
-                )
-              ],
-            ),
+                ),
+              )
+            ],
           ),
-        )
-      );
+        ),
+      ));
     }
 
     return rows;
@@ -235,10 +219,9 @@ class RenderCards {
 
   Widget menuItemList(List<Map<String, dynamic>> snapsot) {
     return Container(
-      child: ListView (
-          scrollDirection: Axis.vertical,
-          children: renderRowArray(snapsot)
-      ),
+      child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Column(children: renderRowArray(snapsot))),
     );
   }
 }
@@ -246,9 +229,9 @@ class RenderCards {
 Widget CardsDashboard(String childDataName) {
   var subject = new PublishSubject<String>();
   var getImage = new GetDateImage();
-  
+
   subject = getImage._readNews(childDataName);
-  
+
   return Container(
     height: 500,
     child: StreamBuilder(
@@ -259,14 +242,12 @@ Widget CardsDashboard(String childDataName) {
         }
 
         var urls = snapsot.data.split(',');
-        
+
         return PageView(
-          controller: PageController(viewportFraction: 1.0),
-          scrollDirection: Axis.horizontal,
-          pageSnapping: true,
-          children: 
-            RenderCards().renderCard(urls)
-        );
+            controller: PageController(viewportFraction: 1.0),
+            scrollDirection: Axis.horizontal,
+            pageSnapping: true,
+            children: RenderCards().renderCard(urls));
       },
     ),
   );
@@ -275,55 +256,50 @@ Widget CardsDashboard(String childDataName) {
 Widget CardsMenu(BuildContext context, String nameBranch, Color colorCard) {
   var subject = new PublishSubject<Map<String, dynamic>>();
   var getImage = new GetDateImage();
-  
+
   subject = getImage._readDate(nameBranch);
 
-  return new GestureDetector (
+  return new GestureDetector(
     onTap: () {
-      Navigator.push(context, MaterialPageRoute(
-        builder: (context) => MenuListPage(nameBranch)
-      ));
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => MenuListPage(nameBranch)));
     },
     child: new Container(
-      height: 150.0,
-      margin: new EdgeInsets.all(10.0),
-      decoration: new BoxDecoration(
-        borderRadius: new BorderRadius.all(new Radius.circular(10.0)),
-        color: colorCard
-      ),
-      child: StreamBuilder(
-        stream: subject.stream,
-        builder: (context, snapsot) {
-          if (snapsot.data == null) {
-            return new Container();
-          }
+        height: 150.0,
+        margin: new EdgeInsets.all(10.0),
+        decoration: new BoxDecoration(
+            borderRadius: new BorderRadius.all(new Radius.circular(10.0)),
+            color: colorCard),
+        child: StreamBuilder(
+          stream: subject.stream,
+          builder: (context, snapsot) {
+            if (snapsot.data == null) {
+              return new Container();
+            }
 
-          return RenderCards().renderRow(snapsot.data);
-        },
-      )
-    ),
+            return RenderCards().renderRow(snapsot.data);
+          },
+        )),
   );
 }
 
 Widget CardsMenuItem(BuildContext context, String nameBranch) {
   var subject = new PublishSubject<List<Map<String, dynamic>>>();
   var getImage = new GetDateImage();
-  
+
   subject = getImage.readDateMenu(nameBranch);
 
-  return new GestureDetector (
-    child: Container (
-      height: 1000,
-      child: StreamBuilder (
-        stream: subject.stream,
-        builder: (context, snapsot) {
-          if (snapsot.data == null) {
-            return new Container();
-          }
+  return new GestureDetector(
+      child: Container(
+          height: 1000,
+          child: StreamBuilder(
+            stream: subject.stream,
+            builder: (context, snapsot) {
+              if (snapsot.data == null) {
+                return new Container();
+              }
 
-          return RenderCards().menuItemList(snapsot.data);
-        },
-      )
-    )
-  );
+              return RenderCards().menuItemList(snapsot.data);
+            },
+          )));
 }
